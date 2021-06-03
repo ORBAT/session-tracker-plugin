9b467b944e17e0ecbfeaa244f0f55ae2ae3fedd6
Tom Eklöf <tom.eklof@gmail.com>
2021-06-03 04:20:16 +0300

    Be consistent about time units

--- index.ts
+++ index.ts
@@ -21,7 +21,7 @@ type SessionTrackerPlugin = Plugin<{
 }>
 
 export const setupPlugin: SessionTrackerPlugin['setupPlugin'] = ({ global, config }) => {
-    global.sessionLength = parseInt(config.sessionLength) || 30
+    global.sessionLength = (parseInt(config.sessionLength) || 30) * 60 * 1000
     global.sessionStartEvent = config.sessionStartEvent || 'Session start'
     global.sessionEndEvent = config.sessionEndEvent || 'Session end'
 }
@@ -32,18 +32,18 @@ export const onEvent: SessionTrackerPlugin['onEvent'] = async (event, { cache, g
         return
     }
     const timestamp = event.timestamp || event.properties?.timestamp || event.now || event.sent_at || new Date().toISOString()
-    // check if we're the first one to increment this key in the last ${global.sessionLength} minutes
+    // check if we're the first one to increment this key in the last ${global.sessionLength} milliseconds
     const num_evts_in_session = await cache.incr(`session_${event.distinct_id}`)
     if (num_evts_in_session === 1) {
         // if so, dispatch a session start event
         posthog.capture(global.sessionStartEvent, { distinct_id: event.distinct_id, timestamp })
-        // and launch a job to check in 30min if the session is still alive
-        await jobs.checkIfSessionIsOver({ distinct_id: event.distinct_id, session_start: timestamp }).runIn(global.sessionLength, 'minutes')
+        // and launch a job to check in sessionLength millis if the session is still alive
+        await jobs.checkIfSessionIsOver({ distinct_id: event.distinct_id, session_start: timestamp }).runIn(global.sessionLength, 'milliseconds')
     }
     // make the key expire in ${global.sessionLength} min
-    await cache.expire(`session_${event.distinct_id}`, global.sessionLength * 60)
+    await cache.expire(`session_${event.distinct_id}`, global.sessionLength)
     await cache.set(
-        `last_seen_${event.distinct_id}`,
+        `seen_last_${event.distinct_id}`,
         timestamp
     )
 
@@ -60,17 +60,19 @@ export const jobs: SessionTrackerPlugin['jobs'] = {
         const ping = await cache.get(`session_${distinct_id}`, undefined)
         if (!ping) {
             // if it expired, dispatch the session end event
-            const last_seen =
-                (await cache.get(`last_seen_${distinct_id}`, undefined)) ||
-                new Date(new Date().valueOf() - global.sessionLength * 60000).toISOString()
-            const duration_seconds = (new Date(last_seen as string).valueOf() - new Date(session_start).valueOf()) * 1000
-            const props: Record<string, any> = { distinct_id, timestamp: last_seen, "duration.seconds": duration_seconds}
+            const seen_last =
+                (await cache.get(`seen_last_${distinct_id}`, undefined)) ||
+                new Date(new Date().valueOf() - global.sessionLength).toISOString()
+            const seen_last_timestamp = new Date(seen_last as string).valueOf()
+            const duration_seconds = (seen_last_timestamp - new Date(session_start).valueOf()) / 1000
+            const seconds_since_last = (Date.now() - seen_last_timestamp) / 1000
+            const props: Record<string, any> = { distinct_id, timestamp: seen_last, "duration.seconds": duration_seconds, "session.start": session_start, "seen.last": seen_last, "seen.last.elapsed-since.seconds": seconds_since_last}
             const num_evts_in_session = (await cache.get(`last_event_count_${distinct_id}`, undefined))
             if(num_evts_in_session) {
                 props["session.events.count"] = num_evts_in_session
             }
 
-            await cache.set(`last_seen_${distinct_id}`, undefined)
+            await cache.set(`seen_last_${distinct_id}`, undefined)
 
             posthog.capture(global.sessionEndEvent, props)
         } else {
